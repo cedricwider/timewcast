fb98019394c6d7e51ea926de316e0c14259ce1f8
Cedric Wider <wider.cedric@gmail.com>
2025-04-15 11:39:05 +0200

    feat (track): add track feature

--- summary/src/track.ts
+++ summary/src/track.ts
@@ -1,11 +1,13 @@
 import { TimewarriorCli } from "./util/timewarrior-cli";
 import { showHUD } from "@raycast/api";
 
-export default async function Command(props: { arguments: { tags: string; startTime?: string } }) {
+export default async function Command(props: { arguments: { startTime: string; endTime: string; tags: string } }) {
   try {
-    const tags = props.arguments.tags.split(",").filter((tag) => tag.length > 0);
     const startTime = props.arguments.startTime;
-    TimewarriorCli.start(tags, startTime);
+    const endTime = props.arguments.endTime;
+    const tags = props.arguments.tags.split(",").filter((tag) => tag.length > 0);
+
+    TimewarriorCli.track(startTime, endTime, tags);
     await showHUD("Started time tracking 🎯");
   } catch (error) {
     await showHUD("Failed to start tracking ❌");
